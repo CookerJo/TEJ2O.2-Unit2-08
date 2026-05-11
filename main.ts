ff8a2f36f6307cb05c67fda1f8549afeeb977197
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-05-11 12:11:24 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  * This program will changed the color of the LED to what the color desplay on the microbit
 */
 
-// setup
+// setup, shows all the color set to the pins
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 pause(500)
@@ -25,61 +25,59 @@ input.onButtonPressed(Button.A, function() {
 
     // red
     basic.clearScreen()
-    basic.showString('Red')
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('Red')    
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 
     // green
     basic.clearScreen()
-    basic.showString('Green')
     pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString('Green')    
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
     
     // blue
     basic.clearScreen()
-    basic.showString('Blue')
-    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString('Blue')    
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
     
     // majenta
     basic.clearScreen()
-    basic.showString('Majent')
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('Majent')    
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     
     // cyan
     basic.clearScreen()
-    basic.showString('Cyan')
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString('Cyan')
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 
     // yellow
     basic.clearScreen()
-    basic.showString('Yellow')
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString('Yellow')
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
 
     // white
     basic.clearScreen()
-    basic.showString('White')
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString('White')
     basic.pause(1000)
-
-    // black
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
